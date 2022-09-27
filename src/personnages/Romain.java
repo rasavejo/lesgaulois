e0d99ac9635a28b902c2e03507acfb0638ea4e95
rumin raphael <rmr4685a@ens.fsi>
2022-09-27 19:45:50 +0200

    Premier push

---
+++ src/personnages/Romain.java
@@ -0,0 +1,37 @@
+package personnages;
+
+public class Romain {
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public int getForce() {
+		return force;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+	}
+	}
+	
+	public static void main(String[] args) {
+		Romain romain = new Romain("Romain",1);
+		romain.parler("Je m'apelle ROMAIN");
+		romain.recevoirCoup(1);
+	}
+}
