26b8d28a0a7898dad0bcf33f864fceef84f47cc7
rumin raphael <rmr4685a@ens.fsi>
2022-10-06 14:28:12 +0200

    pre et post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -5,6 +5,7 @@ public class Romain {
 	private int force;
 	
 	public Romain(String nom, int force) {
+		assert force >= 0;
 		this.nom = nom;
 		this.force = force;
 	}
@@ -21,16 +22,19 @@ public class Romain {
 		return "Le romain " + nom + " : ";
 	}
 	public void recevoirCoup(int forceCoup) {
+		assert force >= 0;
+		int t = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aie");
 		} else {
 			parler("J'abandonne...");
 	}
+		assert t-force > 0;
 	}
 	
 	public static void main(String[] args) {
-		Romain romain = new Romain("Romain",1);
+		Romain romain = new Romain("Romain",6);
 		romain.parler("Je m'apelle ROMAIN");
 		romain.recevoirCoup(1);
 	}
